49234483d3cfc90ca25f6ca0d1334c934b910397
Alex-Willenbrink <awillenbrink@gmail.com>
2017-10-03 13:04:55 -0500

    login page is done

--- client/src/components/LoginForm/index.js
+++ client/src/components/LoginForm/index.js
@@ -1,23 +1,29 @@
-import React, { Component } from 'react';
-import { Field, reduxForm } from 'redux-form';
-import TextField from 'material-ui/TextField';
-import asyncValidate from '../../services/AsyncValidate';
-import CircularProgress from 'material-ui/CircularProgress';
-import { withRouter } from 'react-router-dom';
+import React, { Component } from "react";
+import { Field, reduxForm } from "redux-form";
+import TextField from "material-ui/TextField";
+import asyncValidate from "../../services/AsyncValidate";
+import CircularProgress from "material-ui/CircularProgress";
+import { withRouter } from "react-router-dom";
+
+import "./LoginForm.css";
+
+const style = {
+  color: "white"
+};
 
 const validate = values => {
   const errors = {};
-  const requiredFields = ['username', 'email', 'password'];
+  const requiredFields = ["username", "email", "password"];
   requiredFields.forEach(field => {
     if (!values[field]) {
-      errors[field] = 'Required';
+      errors[field] = "Required";
     }
   });
   if (
     values.email &&
     !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
   ) {
-    errors.email = 'Invalid email address';
+    errors.email = "Invalid email address";
   }
   return errors;
 };
@@ -27,21 +33,20 @@ const renderTextField = ({
   label,
   meta: { touched, error },
   ...custom
-}) => (
+}) =>
   <TextField
     hintText={label}
     floatingLabelText={label}
     errorText={touched && error}
     {...input}
     {...custom}
-  />
-);
+  />;
 
 class LoginForm extends Component {
   state = { error: null };
 
   onSubmit = () => {
-    alert('pressed log in!');
+    alert("pressed log in!");
     // const { login, formData, history } = this.props;
 
     // login(formData.LoginForm.values)
@@ -65,44 +70,53 @@ class LoginForm extends Component {
     }
 
     return (
-      <form onSubmit={handleSubmit(this.onSubmit)}>
-        <h3>Login</h3>
-        <div>
-          <Field
-            name="email"
-            component={renderTextField}
-            label="Email"
-            required="required"
-          />
+      <div>
+        <form onSubmit={handleSubmit(this.onSubmit)}>
+          <p className="label">login</p>
+          <div>
+            <Field
+              className="material-field"
+              name="email"
+              component={renderTextField}
+              label="Email"
+              required="required"
+            />
+          </div>
+          <div>
+            <Field
+              className="material-field"
+              name="password"
+              type="password"
+              component={renderTextField}
+              label="Password"
+              required="required"
+            />
+          </div>
+          <div className="login-buttons">
+            <button type="submit" disabled={pristine || submitting}>
+              Login
+            </button>
+            <button
+              type="button"
+              disabled={pristine || submitting}
+              onClick={reset}
+            >
+              Reset
+            </button>
+          </div>
+        </form>
+        <div className="oauth">
+          <p className="label">or</p>
+          <img src="https://imgur.com/Hw9YUrJ.png" />
+          <img src="https://i.imgur.com/ETp8DOT.png" />
         </div>
-        <div>
-          <Field
-            name="password"
-            type="password"
-            component={renderTextField}
-            label="Password"
-            required="required"
-          />
-        </div>
-        <div>
-          <button type="submit" disabled={pristine || submitting}>
-            Login
-          </button>
-          <button
-            type="button"
-            disabled={pristine || submitting}
-            onClick={reset}
-          >
-            Reset
-          </button>
-        </div>
-      </form>
+      </div>
     );
   }
 }
 
 export default reduxForm({
-  form: 'LoginForm',
+  form: "LoginForm",
   validate,
   asyncValidate
 })(withRouter(LoginForm));
