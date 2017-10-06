3ef70a4a98f43c6b429538f09af9cf73cc413203
Alex-Willenbrink <awillenbrink@gmail.com>
2017-10-06 12:26:13 -0500

    a few changes

--- client/src/components/SearchRecipes/index.js
+++ client/src/components/SearchRecipes/index.js
@@ -22,78 +22,130 @@ import StarRatingComponent from "react-star-rating-component";
 
 class SearchRecipes extends Component {
   state = {
-    // sortFilter: null,
-    // sortValue: 1,
-    // dietaryFilter: null,
-    // dietaryValue: 1,
     recipes: [],
-    // healthValue: 1,
     healthTokens: [],
     healthOptions: [
-      { id: 1, name: "vegan", element: <span>vegan</span> }
-      // { id: 2, name: "vegetarian", element: <span>vegetarian</span> },
-      // { id: 3, name: "sugar-conscious", element: <span>sugar-conscious</span> },
-      // { id: 4, name: "peanut-free", element: <span>peanut-free</span> },
-      // { id: 5, name: "tree-nut-free", element: <span>tree-nut-free</span> },
-      // { id: 5, name: "alcohol-free", element: <span>alcohol-free</span> }
-    ]
+      { id: 1, name: "vegan", element: <span>vegan</span> },
+      { id: 2, name: "vegetarian", element: <span>vegetarian</span> },
+      { id: 3, name: "sugar-conscious", element: <span>sugar-conscious</span> },
+      { id: 4, name: "peanut-free", element: <span>peanut-free</span> },
+      { id: 5, name: "tree-nut-free", element: <span>tree-nut-free</span> },
+      { id: 6, name: "alcohol-free", element: <span>alcohol-free</span> }
+    ],
+    healthFilters: [],
+    dietTokens: [],
+    dietOptions: [
+      { id: 1, name: "balanced", element: <span>balanced</span> },
+      { id: 2, name: "high-protein", element: <span>high-protein</span> },
+      { id: 3, name: "low-fat", element: <span>low-fat</span> },
+      { id: 4, name: "low-carb", element: <span>low-carb</span> }
+    ],
+    dietFilters: []
   };
 
   componentWillReceiveProps(nextProps) {
+    console.log("nextProps: ", nextProps);
     this.setState({
       recipes: !Array.isArray(nextProps.searchReducer.results)
         ? []
         : nextProps.searchReducer.results
     });
     if (nextProps.searchReducer.query !== this.props.searchReducer.query) {
-      this.props.searchActions.requestSearch(nextProps.searchReducer.query);
+      this.props.searchActions.requestSearch(
+        nextProps.searchReducer.query,
+        nextProps.searchReducer.preferences
+      );
     }
   }
 
   componentWillMount() {
-    this.props.searchActions.requestSearch(this.props.searchReducer.query);
+    this.props.searchActions.requestSearch(
+      this.props.searchReducer.query,
+      this.props.searchReducer.preferences
+    );
   }
 
   // handleChange = (event, index, value) => this.setState({ value });
 
-  selectHealthToken = ({ target: { value: healthTokens } }) => {
-    this.setState({ healthTokens });
+  selectToken = e => {
+    const filterType = e.target.name;
+    const filterTokenArray = e.target.value;
+    this.setState({
+      [`${filterType}Tokens`]: filterTokenArray,
+      [`${filterType}Filters`]: filterTokenArray.map(
+        token => this.state[`${filterType}Options`][token - 1].name
+      )
+    });
   };
 
-  renderInputToken = () =>
+  filterRecipes = () => {
+    return this.state.recipes.filter(recipe => {
+      return this.isValidRecipe(recipe);
+    });
+  };
+
+  isValidRecipe = recipe => {
+    const filters = [...this.state.dietFilters, ...this.state.healthFilters];
+    console.log("filters: ", filters);
+    return filters.reduce(
+      (isValid, filter) =>
+        recipe.preferences &&
+        !!recipe.preferences.find(
+          preference => preference.toLowerCase() === filter.toLowerCase()
+        ) &&
+        isValid,
+      true
+    );
+  };
+
+  renderHealthInputToken = () =>
     <InputToken
-      name="healthFilters"
+      name="health"
       value={this.state.healthTokens}
       placeholder="pick health option"
       options={this.state.healthOptions}
       onSelect={this.selectToken}
     />;
 
+  renderDietInputToken = () =>
+    <InputToken
+      name="diet"
+      value={this.state.dietTokens}
+      placeholder="pick diet option"
+      options={this.state.dietOptions}
+      onSelect={this.selectToken}
+    />;
+
   render() {
-    const recipes = this.state.recipes
-      ? this.state.recipes.map(recipe =>
-          <Card className="recipe-card">
-            <CardMedia>
-              {recipe.image && <img src={recipe.image.url} />}
-            </CardMedia>
-            <CardTitle className="card-title">
-              {recipe.name}
-            </CardTitle>
-            <StarRatingComponent
-              className="star-rating"
-              name="rating"
-              value={Math.floor(Math.random() * 5)}
-              editing={false}
-            />
-          </Card>
-        )
-      : null;
+    const recipes = this.filterRecipes(this.state.recipes).map(recipe =>
+      <Card className="recipe-card" key={recipe.name}>
+        <CardMedia>
+          {recipe.image && <img src={recipe.image.url} />}
+        </CardMedia>
+        <CardTitle className="card-title">
+          {recipe.name}
+        </CardTitle>
+        <StarRatingComponent
+          className="star-rating"
+          name="rating"
+          value={Math.floor(Math.random() * 5)}
+          editing={false}
+        />
+      </Card>
+    );
     return (
       <div className="background">
         <div className="search-container">
           <div className="search-recipes">
             <p className="search-recipes-title">find delicious recipes</p>
-            {this.renderInputToken()}
+            <div className="filters-container">
+              <div className="filter filter-health">
+                {this.renderHealthInputToken()}
+              </div>
+              <div className="filter filter-diet">
+                {this.renderDietInputToken()}
+              </div>
+            </div>
             <div className="recipe-results">
               {this.props.searchReducer.isSearching
                 ? <CircularProgress />
