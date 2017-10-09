7109292e0f243d72b8a6052bb9bb1c807ef4a786
Greg Filipczak <gregfilipczak@gmail.com>
2017-10-09 23:35:04 +0000

    update

--- client/src/components/MealContainer/Meal.js
+++ client/src/components/MealContainer/Meal.js
@@ -9,6 +9,26 @@ const Meal = ({ meal }) =>
     <h3>
       This delicious meal hosted by {meal.owner.username}
     </h3>
+    {meal.registeredGuests || meal.unregisteredGuests
+      ? <div>
+          <ul>
+            {meal.registeredGuests.length
+              ? meal.registeredGuests.map(guest =>
+                  <li>
+                    {guest.username}
+                  </li>
+                )
+              : null}
+            {meal.unregisteredGuests.length
+              ? meal.unregisteredGuests.map(guest =>
+                  <li>
+                    {guest.username}
+                  </li>
+                )
+              : null}
+          </ul>
+        </div>
+      : null}
   </div>;
 
 export default Meal;
