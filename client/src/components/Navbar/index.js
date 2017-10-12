2375c1ac33f952b86b5f59aaee5967e4c4b182be
Alex-Willenbrink <awillenbrink@gmail.com>
2017-10-12 10:08:52 -0500

    fixed intial search recipes glitch

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,7 +1,11 @@
 import React, { Component } from "react";
 
 // import AccountCircle from "./ic_account_circle_black_24px.svg";
-// import SvgIcon from "material-ui/SvgIcon";
+import SvgIcon from "material-ui/SvgIcon";
+import FloatingActionButton from "material-ui/FloatingActionButton";
+import IconButton from "material-ui/IconButton";
+// import ChickenSvg from "./chicken";
+// import Login from "./login";
 
 import * as userActions from "../../actions/user_actions";
 import * as recipesActions from "../../actions/recipes_actions";
@@ -115,6 +119,15 @@ class Navbar extends Component {
   render() {
     let navItems = [];
     // navItems.push(<ProfileIcon color="#FAFAFA" />);
+    navItems.push(
+      <SvgIcon>
+        <path d="M367.282,78.106c0,14.692-11.954,26.645-26.645,26.645H178.155c-13.252,0-24.033,10.78-24.033,24.033
+  			s10.78,24.033,24.033,24.033h75.233c9.218,0,16.718,7.5,16.718,16.718c0,9.218-7.5,16.718-16.718,16.718H145.241
+  			c-24.487,0-44.408,19.921-44.408,44.408h15.673c0-15.844,12.891-28.735,28.735-28.735h108.147
+  			c17.862,0,32.392-14.53,32.392-32.392c0-17.862-14.53-32.392-32.392-32.392h-75.233c-4.609,0-8.359-3.75-8.359-8.359
+  			s3.75-8.359,8.359-8.359h162.482c23.335,0,42.318-18.984,42.318-42.318H367.282z" />
+      </SvgIcon>
+    );
 
     switch (this.props.location.pathname) {
       case "/login":
